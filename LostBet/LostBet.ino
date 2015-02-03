5fd925645e9fe9599771ec80b0f62165ddc83945
LoadingPleaseWait <michael@murphey.org>
2015-02-03 17:51:30 -0600

    added source file

---
+++ LostBet/LostBet.ino
@@ -0,0 +1,17 @@
+const int motorPort = 3;
+const int inputPort = 3;
+
+/*
+ * set up the ports
+ */
+void setup(){
+  pinMode(motorPort, OUTPUT);
+  pinMode(inputPort, INPUT);
+}
+
+/*
+ *
+ */
+void loop(){
+  
+}
