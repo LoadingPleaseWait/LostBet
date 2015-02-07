358f99ea0acd486bd351a7e55aeb812d1d4b41be
LoadingPleaseWait <michael@murphey.org>
2015-02-06 23:26:04 -0600

    declared pin number constants and set pin modes in setup

--- LostBet/LostBet.ino
+++ LostBet/LostBet.ino
@@ -1,16 +1,34 @@
-const int motorPort = 3;
-const int inputPort = 3;
+//declare pin number constants
+const int JAGUAR_SIGNAL= 3;
+const int JAGUAR_POWER = 3;
+const int JAGUAR_GROUND = 3;
+const int FORWARD_INPUT = 3;
+const int BACK_INPUT = 3;
+const int LEFT_INPUT = 3;
+const int RIGHT_INPUT = 3;
 
 /*
  * set up the ports
  */
 void setup(){
-  pinMode(motorPort, OUTPUT);
-  pinMode(inputPort, INPUT);
+  // output ports
+  pinMode(JAGUAR_SIGNAL, OUTPUT);
+  pinMode(JAGUAR_POWER, OUTPUT);
+  pinMode(JAGUAR_GROUND, OUTPUT);
+  
+  // power is 5V ground is 0V
+  digitalWrite(JAGUAR_POWER, HIGH);
+  digitalWrite(JAGUAR_GROUND, LOW);
+  
+  // input ports
+  pinMode(FORWARD_INPUT, INPUT);
+  pinMode(BACK_INPUT, INPUT);
+  pinMode(LEFT_INPUT, INPUT);
+  pinMode(RIGHT_INPUT, INPUT);
 }
 
 /*
- *
+ * Drive with the joystick input
  */
 void loop(){
   
