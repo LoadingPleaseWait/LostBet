052020750127a44aa87ad49c4bf2724855570152
LoadingPleaseWait <michael@murphey.org>
2015-02-13 21:41:51 -0600

    added left and right jaguars

--- LostBet/LostBet.ino
+++ LostBet/LostBet.ino
@@ -1,7 +1,10 @@
 //declare pin number constants
-const int JAGUAR_SIGNAL= 3;
-const int JAGUAR_POWER = 3;
-const int JAGUAR_GROUND = 3;
+const int LEFT_JAGUAR_SIGNAL= 3;
+const int LEFT_JAGUAR_POWER = 3;
+const int LEFT_JAGUAR_GROUND = 3;
+const int RIGHT_JAGUAR_SIGNAL = 3;
+const int RIGHT_JAGUAR_POWER = 3;
+const int RIGHT_JAGUAR_GROUND = 3;
 const int FORWARD_INPUT = 3;
 const int BACK_INPUT = 3;
 const int LEFT_INPUT = 3;
@@ -12,13 +15,18 @@ const int RIGHT_INPUT = 3;
  */
 void setup(){
   // output ports
-  pinMode(JAGUAR_SIGNAL, OUTPUT);
-  pinMode(JAGUAR_POWER, OUTPUT);
-  pinMode(JAGUAR_GROUND, OUTPUT);
+  pinMode(LEFT_JAGUAR_SIGNAL, OUTPUT);
+  pinMode(LEFT_JAGUAR_POWER, OUTPUT);
+  pinMode(LEFT_JAGUAR_GROUND, OUTPUT);
+  pinMode(RIGHT_JAGUAR_SIGNAL, OUTPUT);
+  pinMode(RIGHT_JAGUAR_POWER, OUTPUT);
+  pinMode(RIGHT_JAGUAR_GROUND, OUTPUT);
   
-  // power is 5V ground is 0V
-  digitalWrite(JAGUAR_POWER, HIGH);
-  digitalWrite(JAGUAR_GROUND, LOW);
+  // power is high voltage ground is low voltage
+  digitalWrite(LEFT_JAGUAR_POWER, HIGH);
+  digitalWrite(RIGHT_JAGUAR_POWER, HIGH);
+  digitalWrite(LEFT_JAGUAR_GROUND, LOW);
+  digitalWrite(RIGHT_JAGUAR_GROUND, LOW);
   
   // input ports
   pinMode(FORWARD_INPUT, INPUT);
